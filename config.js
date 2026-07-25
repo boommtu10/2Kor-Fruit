e2554a6251e07cad305d565fadf42a6b7bfe12ca
boommtu10 <boommtu10@gmail.com>
2026-07-25 18:21:05 +0700

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // วาง Web App URL ที่ได้จากการ Deploy ไฟล์ Code.gs ตรงนี้
 // ตัวอย่าง: "https://script.google.com/macros/s/AKfycb.../exec"
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyRXTJ4rgSjZ6VDBrFaI84AvuQ2GUlqB1aG2cIrGeyJJyHCkIDgA3jmugTqO_Umo_YI/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyA8wtYKeD5CNaObGYhg4AgVmLAPrXdBFOtzmjoSx4XOnD3DuWIUkuOaL4dfWpWeAxv/exec";
