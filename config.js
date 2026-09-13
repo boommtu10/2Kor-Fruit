29f5233afd0f6fbd45daabcf6f7e36201c964932
boommtu10 <boommtu10@gmail.com>
2026-09-13 15:43:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// วาง Web App URL ที่ได้จากการ Deploy ไฟล์ Code.gs ตรงนี้
+// ตัวอย่าง: "https://script.google.com/macros/s/AKfycb.../exec"
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxZJoylhr-xSs5EJoNwffcESPApeQ489nDav3_KtZumfftZwT8iuXZxmI5gfjQTyDra/exec";
