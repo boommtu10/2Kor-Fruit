94b8cf9435bf619f0fc6c86f79f1ab398d3c286f
boommtu10 <boommtu10@gmail.com>
2026-09-25 17:18:51 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// วาง Web App URL ที่ได้จากการ Deploy ไฟล์ Code.gs ตรงนี้
-// ตัวอย่าง: "https://script.google.com/macros/s/AKfycb.../exec"
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxcKYB3_NB1ym5coRqL4dWsLXBGqZvWwXiwyV-lIhbSqDMiEEYH7WuWApnuYNs13Gu9/exec";
